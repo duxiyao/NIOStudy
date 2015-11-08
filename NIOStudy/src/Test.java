4568d3bb7813b3d43ae47040f41618fc5cc74707
duxiyao <373071116@qq.com>
2015-11-08 15:07:44 +0800

    ...

---
+++ NIOStudy/src/Test.java
@@ -0,0 +1,72 @@
+public class Test {
+
+	public static void main(String[] args) {
+		// System.out.println(Integer.toHexString(27));
+		byte[] b = new byte[] { 1, 2, 3, 4, 5, 14, 15, 16 };
+		String s=bytesToHexString(b);
+		System.out.println(s);
+		
+		String str="0x0x";
+		byte[] bb=hexStringToBytes(str);
+		for(byte t:bb){
+			System.out.println(t);
+		}
+	}
+
+	/**
+	 * Convert byte[] to hex
+	 * string.这里我们可以将byte转换成int，然后利用Integer.toHexString(int)来转换成16进制字符串。
+	 * 
+	 * @param src
+	 *            byte[] data
+	 * @return hex string
+	 */
+	public static String bytesToHexString(byte[] src) {
+		StringBuilder stringBuilder = new StringBuilder("");
+		if (src == null || src.length <= 0) {
+			return null;
+		}
+		for (int i = 0; i < src.length; i++) {
+			int v = src[i] & 0xFF;
+			String hv = Integer.toHexString(v);
+			if (hv.length() < 2) {
+				stringBuilder.append(0);
+			}
+			stringBuilder.append(hv);
+		}
+		return stringBuilder.toString();
+	}
+
+	/**
+	 * Convert hex string to byte[]
+	 * 
+	 * @param hexString
+	 *            the hex string
+	 * @return byte[]
+	 */
+	public static byte[] hexStringToBytes(String hexString) {
+		if (hexString == null || hexString.equals("")) {
+			return null;
+		}
+		hexString = hexString.toUpperCase();
+		int length = hexString.length() / 2;
+		char[] hexChars = hexString.toCharArray();
+		byte[] d = new byte[length];
+		for (int i = 0; i < length; i++) {
+			int pos = i * 2;
+			d[i] = (byte) (charToByte(hexChars[pos]) << 4 | charToByte(hexChars[pos + 1]));
+		}
+		return d;
+	}
+
+	/**
+	 * Convert char to byte
+	 * 
+	 * @param c
+	 *            char
+	 * @return byte
+	 */
+	private static byte charToByte(char c) {
+		return (byte) "0123456789ABCDEF".indexOf(c);
+	}
+}
